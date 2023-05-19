573b4754930ef7f2cddf535b96f37bbfa175becc
flvSantos15 <flvSantos300@gmail.com>
2023-05-18 23:33:54 -0300

    feat: upload route

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,11 @@ import 'dotenv/config'
 import fastify from 'fastify'
 import cors from '@fastify/cors'
 import jwt from '@fastify/jwt'
+import multipart from '@fastify/multipart'
 import { memoriesRoutes } from './routes/memories'
 import { authRoutes } from './routes/auth'
+import { uploadRoutes } from './routes/upload'
+import { resolve } from 'node:path'
 
 const app = fastify()
 
@@ -14,13 +17,20 @@ app.register(cors, {
 app.register(jwt, {
   secret: 'spacetime'
 })
+app.register(multipart)
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads'
+})
 
 app.register(memoriesRoutes)
 app.register(authRoutes)
+app.register(uploadRoutes)
 
 app
   .listen({
-    port: 3333
+    port: 3333,
+    host: '::'
   })
   .then(() => {
     console.log('HTTP server running on http://localhost:3333')
